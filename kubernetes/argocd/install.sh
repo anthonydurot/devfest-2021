b09dda9618d14d6e303c35039f9c93bdca2e18ff
Anthony Durot <adurot.pro@gmail.com>
2021-10-31 19:05:40 +0100

    Changmeent script d'instal ArgoCD + ajout script config kubectl

--- kubernetes/argocd/install.sh
+++ kubernetes/argocd/install.sh
@@ -1,2 +1,5 @@
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+echo "ArgoCD password :"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d && echo
+kubectl port-forward svc/argocd-server -n argocd 8080:443
---
+++ terraform/set_kubectl.sh
@@ -0,0 +1,1 @@
+gcloud container clusters get-credentials $(terraform output -raw kubernetes_cluster_name) --region $(terraform output -raw region)
