8a352092970d0c6872c99d90a487277b99aa9f98
Anthony Durot <adurot.pro@gmail.com>
2021-11-06 14:42:21 +0100

    WIP : Mise en place Spring Vault + déploiement Terraform de Vault

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,2 +1,8 @@
 project_id = "dark-gateway-330714"
 region     = "us-central1"
+
+kms_location = "us-central1"
+kms_keyring = "vault"
+kms_crypto_key= "vault-init"
+load_balancing_scheme = "INTERNAL"
+allow_public_egress = "true"
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -24,20 +24,22 @@ resource "google_compute_subnetwork" "subnet" {
 
 
 
-# # Obligatoire pour mettre la DB dans un réseau privé
-# resource "google_compute_global_address" "private_ip_address" {
-#   provider = google-beta
+# Obligatoire pour mettre la DB dans un réseau privé
+resource "google_compute_global_address" "private_ip_address" {
+  provider = google-beta
 
-#   name          = "private-ip-address"
-#   purpose       = "VPC_PEERING"
-#   address_type  = "INTERNAL"
-#   network       = google_compute_network.vpc.id
-# }
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  network       = google_compute_network.vpc.id
+}
 
-# resource "google_service_networking_connection" "private_vpc_connection" {
-#   provider = google-beta
+resource "google_service_networking_connection" "private_vpc_connection" {
+  provider = google-beta
 
-#   network                 = google_compute_network.vpc.id
-#   service                 = "servicenetworking.googleapis.com"
-#   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-# }
+  network                 = google_compute_network.vpc.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+# TODO : Ajouter pare feu SSH + Mysql
