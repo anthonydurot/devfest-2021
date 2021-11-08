8eaffa780b69998059271d066d7f58a749417055
Anthony Durot <adurot.pro@gmail.com>
2021-11-08 11:39:43 +0100

    Retrait annotation Bean getSecret()

--- application/src/main/java/com/ensat/repositories/DatasourceConfig.java
+++ application/src/main/java/com/ensat/repositories/DatasourceConfig.java
@@ -17,8 +17,11 @@ public class DatasourceConfig {
         try {
             DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
             dataSourceBuilder.url(secretManager.getSecret("dark-gateway-330714", "database-url", "latest"));
+            System.out.println("URL récupérée");
             dataSourceBuilder.username(secretManager.getSecret("dark-gateway-330714", "database-username", "latest"));
+            System.out.println("Username récupéré");
             dataSourceBuilder.password(secretManager.getSecret("dark-gateway-330714", "database-password", "latest"));
+            System.out.println("Password récupéré");
             return dataSourceBuilder.build();
         } catch (Exception e) {
             System.out.println(e);
--- application/src/main/java/com/ensat/utils/SecretManager.java
+++ application/src/main/java/com/ensat/utils/SecretManager.java
@@ -6,14 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecretManager {
-    @Bean
     public String getSecret(String projectId, String secretName, String secretVersion) throws Exception {
 
         try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
 
           SecretVersionName secretVersionName = SecretVersionName.of(projectId, secretName, secretVersion);
           AccessSecretVersionResponse secretVersionResponse = client.accessSecretVersion(secretVersionName);
-          System.out.printf("Database URL: %s\n", secretVersionResponse.getPayload().getData().toStringUtf8());
           return secretVersionResponse.getPayload().getData().toStringUtf8();
         }
     }
