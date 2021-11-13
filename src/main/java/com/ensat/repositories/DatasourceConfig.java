01493111068cb61d473540a5fac3e0fb2147e28f
Anthony Durot <adurot.pro@gmail.com>
2021-11-13 11:41:58 +0100

    Test connexion DB

--- src/main/java/com/ensat/repositories/DatasourceConfig.java
+++ src/main/java/com/ensat/repositories/DatasourceConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class DatasourceConfig {
@@ -21,9 +22,11 @@ public class DatasourceConfig {
         try {
             AccessToken accessToken = databaseCredentials.getAccessToken();
             DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-            dataSourceBuilder.url("jdbc:mysql:///devfest_database_iam");
-            dataSourceBuilder.username("evfest@dark-gateway-330714.iam.gserviceaccount.com");
-            dataSourceBuilder.password(accessToken.toString());
+            String db_name = "devfest_database_iam";
+            String db_instance = " devfest-database-instance-new ";
+            String db_username = "devfest-sql-user@dark-gateway-330714.iam.gserviceaccount.com";
+            String db_password = accessToken.toString();
+            dataSourceBuilder.url(String.format("jdbc:mysql:///%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=%s&password=%s", db_name, db_instance, db_username, db_password));
             return dataSourceBuilder.build();
         } catch (Exception e) {
             System.out.println(e);
--- src/main/java/com/ensat/utils/DatabaseCredentials.java
+++ src/main/java/com/ensat/utils/DatabaseCredentials.java
@@ -7,12 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.services.sqladmin.SQLAdminScopes;
 
+import java.io.FileInputStream;
+import java.util.Collections;
+
 @Configuration
 public class DatabaseCredentials {
     public AccessToken getAccessToken() throws Exception {
-        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
+        GoogleCredentials credentials = GoogleCredentials.getApplicationDefault().createScoped(Collections.singleton(SQLAdminScopes.SQLSERVICE_ADMIN));
+        credentials.refresh();
+        credentials.refreshAccessToken();
         AccessToken accessToken = credentials.getAccessToken();
-        System.out.println(accessToken);
         return accessToken;
     }
 }
