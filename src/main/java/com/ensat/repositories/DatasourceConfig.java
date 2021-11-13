d839b1ae0c0bc641b1c36d88c5d9e92af377a190
Anthony Durot <adurot.pro@gmail.com>
2021-11-13 14:46:49 +0100

    Ajout GCP MySQL 8 socket factory

--- src/main/java/com/ensat/repositories/DatasourceConfig.java
+++ src/main/java/com/ensat/repositories/DatasourceConfig.java
@@ -23,10 +23,12 @@ public class DatasourceConfig {
             AccessToken accessToken = databaseCredentials.getAccessToken();
             DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
             String db_name = "devfest_database_iam";
-            String db_instance = " devfest-database-instance-new ";
+            String db_instance = "dark-gateway-330714:us-central1:devfest-database-instance-new";
             String db_username = "devfest-sql-user@dark-gateway-330714.iam.gserviceaccount.com";
             String db_password = accessToken.toString();
-            dataSourceBuilder.url(String.format("jdbc:mysql:///%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=%s&password=%s", db_name, db_instance, db_username, db_password));
+            dataSourceBuilder.url(String.format("jdbc:mysql:///%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory", db_name, db_instance));
+            dataSourceBuilder.username(db_username);
+            dataSourceBuilder.password(db_password);
             return dataSourceBuilder.build();
         } catch (Exception e) {
             System.out.println(e);
