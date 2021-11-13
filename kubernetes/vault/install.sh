a4157129341698fec611a528334a692e5c46dece
Anthony Durot <adurot.pro@gmail.com>
2021-11-13 18:01:20 +0100

    Reorganisation

--- kubernetes/vault/install.sh
+++
@@ -1,6 +0,0 @@
-kubectl create namespace vault
-helm repo add hashicorp https://helm.releases.hashicorp.com
-helm install vault hashicorp/vault \    
---namespace vault \    
---set "server.dev.enabled=true" \
---dry-run
--- terraform/providers.tf
+++
@@ -1,8 +0,0 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-provider "google-beta" {
-  region  = var.region
-}
